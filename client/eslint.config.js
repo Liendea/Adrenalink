c8caa774ce2d9d4157ef0362725f9ce64692ad2f
Linda <bengtsson-linda@outlook.com>
2026-05-11 21:04:33 +0200

    eslint installation

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,22 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist", "node_modules", ".next"]), // Ignorera mappar vi inte vill linta
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended, // Bra grundregler för React
+      reactHooks.configs.recommended, // Viktigt för att useEffect/useState ska funka rätt
     ],
     languageOptions: {
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/react-in-jsx-scope": "off", // Behövs inte i modern React
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
-])
+]);
